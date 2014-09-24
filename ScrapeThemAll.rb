7f53156e926ce9de55682fbef8f4874d0ce4ad88
Maxim Veksler <maxim@vekslers.org>
2014-09-24 03:41:29 +0300

    Nice

--- ScrapeThemAll.rb
+++ ScrapeThemAll.rb
@@ -2,14 +2,8 @@ require_relative 'scraping_joy'
 
 ROOT = '/Volumes/Macintosh SD/apple_source_code_download'
 
-# ios_samples_scraper = ScrapingJoy.new(ROOT, 'iOS', prerelease=true)
-# ios_samples_scraper.scrape()
+ScrapingJoy.new(ROOT, 'Mac').scrape()
+ScrapingJoy.new(ROOT, 'iOS').scrape()
 
-ios_samples_scraper = ScrapingJoy.new(ROOT, 'iOS')
-ios_samples_scraper.scrape()
-
-# ios_samples_scraper = ScrapingJoy.new(ROOT, 'Mac', prerelease=true)
-# ios_samples_scraper.scrape()
-
-ios_samples_scraper = ScrapingJoy.new(ROOT, 'Mac')
-ios_samples_scraper.scrape()
+ScrapingJoy.new(ROOT, 'Mac', prerelease=true).scrape()
+ScrapingJoy.new(ROOT, 'iOS', prerelease=true).scrape()
